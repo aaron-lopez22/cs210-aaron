68155fac646d0dbd046f4ee3e006bd1e62a62987
aaron-lopez22 <aaron.a.lopez22@gmail.com>
2023-06-10 17:25:41 -0600

    prep

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,47 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What is your grade percentage in the class? ");
+        string userInput = Console.ReadLine();
+        int number = int.Parse(userInput);
+
+        int gradeA = 90;
+        int gradeB = 80;
+        int gradeC = 70;
+        int gradeD = 60;
+        string letter= "";
+        string message = "";
+
+        if (number >= gradeA)
+        {
+           letter = "A";
+        }
+        else if (number >= gradeB)
+        {
+            letter = "B";
+        }
+        else if (number >= gradeC)
+        {
+            letter = "C";
+        }
+        else if (number >= gradeD)
+        {
+           letter = "D";
+        }
+        else
+        {
+            letter = "F";
+        }
+
+        if (number < gradeC)
+        {
+            message = "Sorry you did not pass the class.";
+        }
+        else
+        {
+            message = "Congrats you passed the class";
+        }
+
+        Console.WriteLine($"Your grade is {letter}. {message}");
     }
 }
