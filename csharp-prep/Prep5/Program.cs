fc4d6a043776ce675848482db6e209cdefc4f85d
aaron-lopez22 <aaron.a.lopez22@gmail.com>
2023-06-10 19:27:37 -0600

    prep5

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -4,6 +4,43 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep5 World!");
+
+        DisplayMessage();
+        string name = PromptUserName();
+        int number=  PromptUserNumber();
+        double square = SquareNumber(number);
+        DisplayResult(name, square);
+    
+
+        static void DisplayMessage()
+        {
+        Console.WriteLine("Welcome to the program!");
+        }
+
+        static string PromptUserName()
+        {
+            Console.Write("Please enter your name: ");
+            string userName = Console.ReadLine();
+            return userName;
+        }
+
+        static int PromptUserNumber()
+        {
+            Console.Write("Please enter your favorite number: ");
+            string num = Console.ReadLine();
+            int userNumber = int.Parse(num);
+            return userNumber;
+        }
+
+        static double SquareNumber(double number)
+        {
+            double square = number * number;
+            return square;
+        }
+
+        static void DisplayResult(string name, double number)
+        {
+            Console.WriteLine($"{name}, the square of your number is {number}");
+        }
     }
 }
