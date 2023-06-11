97672bb1afb0e776b41cf46207f37ff90208bf32
aaron-lopez22 <aaron.a.lopez22@gmail.com>
2023-06-10 18:04:01 -0600

    proj3

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,32 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Random randomGenerator = new Random();
+        int magicNumber = randomGenerator.Next(1, 101);
+
+        Console.Write("What is your guess? ");
+        string guess = Console.ReadLine();
+        int userGuess = int.Parse(guess);
+
+        while (userGuess != magicNumber)
+        {
+            if (userGuess > magicNumber)
+            {
+                Console.WriteLine("Lower");
+                Console.Write("What is your guess? ");
+                guess = Console.ReadLine();
+                userGuess = int.Parse(guess);
+
+            }
+            else
+            {
+                Console.WriteLine("Higher");
+                Console.Write("What is your guess? ");
+                guess = Console.ReadLine();
+                userGuess = int.Parse(guess);
+            }
+        }
+
+        Console.Write("Congrats you guessed it!");
     }
 }
