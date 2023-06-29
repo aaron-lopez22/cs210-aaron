3d1c4e32d6c1d8c042472192e191d355189f9854
aaron-lopez22 <aaron.a.lopez22@gmail.com>
2023-06-28 18:50:25 -0600

    proj3

--- prove/Develop03/Program.cs
+++ prove/Develop03/Program.cs
@@ -4,6 +4,19 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Develop03 World!");
+        string userInput = "";
+
+        Reference reference = new Reference("Proverbs", 3, 5, 6);
+        Scripture scripture = new Scripture(reference, "Trust in the lord with all thine heart and lean not unto thine own understanding. In all they ways acknowledge him, and he shall direct thy paths.");
+
+        while (userInput != "quit")
+        {
+            Console.WriteLine(scripture.GetDisplayText());
+            Console.WriteLine();
+            Console.WriteLine("Press Enter to continue or type 'quit' to finish");
+            userInput = Console.ReadLine();
+            Console.Clear();
+        }
+
     }
 }
